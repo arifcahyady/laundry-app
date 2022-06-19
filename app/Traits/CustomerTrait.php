0d76a607129dca9dc604b9524fbb877f52fe188d
arif cahyady <arifcihoy@gmail.com>
2022-06-19 08:08:47 +0700

    create admin trait

--- app/Traits/CustomerTrait.php
+++ app/Traits/CustomerTrait.php
@@ -14,27 +14,6 @@ trait CustomerTrait
     protected function getProfileCustomer()
     {
         $customer = User::where('id', Auth::id())->first();
-
-        if (!$token = auth('api')->attempt($customer)) {
-            return $this->errorResponse('Unauthorized user login', 422);
-        }
-
         return $this->successResponse($customer, 'Success', 200);
-    }
-
-    protected function confirmBooking(Request $request, $id)
-    {
-        if (!$booking = Booking::find($id)) {
-            return $this->notFoundResponse('booking');
-        }
-
-        $category = Category::where('id', $booking->laundry_type_id)->first();
-        $booking->laundry_type_id   = $category->id;
-        $booking->weight = round($request->weight);
-        $booking->status = true;
-        $booking->price = $category->price * $request->weight;
-        $booking->save();
-
-        return $this->successResponse($booking, 'Booking successfully confirmed', 200);
     }
 }
