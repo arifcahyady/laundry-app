dfa00bc2ccb81b94c9e5a31cdedbee7dc22df205
arif cahyady <arifcihoy@gmail.com>
2022-06-19 09:23:05 +0700

    create customer trait

---
+++ app/Traits/BookingTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Booking;
+use App\Models\Category;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+
+trait AdminTrait
+{
+    use ApiResponser;
+
+    protected function getUserBooking()
+    {
+        $booking = Booking::where('user_id', auth()->user()->id)->get();
+        return $this->successResponse($booking, 'Success');
+    }
+
+    protected function createBooking(Request $request)
+    {
+        $category = Category::where('id', $request->laundry_type_id)->first();
+
+        $request->validate([
+            'laundry_type_id' => 'required',
+            'address' => 'required',
+
+        ]);
+
+        $booking = new Booking;
+        $booking->user_id = auth()->user()->id;
+        $booking->laundry_type_id = $request->laundry_type_id;
+        $booking->date = Carbon::now();
+        $booking->address = $request->address;
+        $booking->laundry_type = $category->laundry_type;
+        $booking->save();
+
+        return $this->successResponse($booking, 'Booking has been created successfully');
+    }
+}
